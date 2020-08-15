4ddd141234121e2185738c29322914596994f556
Guilherme Manzano <guilherme.manzano93@gmail.com>
2020-08-15 19:12:58 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -33,6 +33,12 @@ public class ChessMatch {
 		return (ChessPiece)capturedPiece;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();		
+	}
+	
 	private void validateTargetPosition(Position source, Position target) {
 		if (!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The chosen piece can't move to the target position");
